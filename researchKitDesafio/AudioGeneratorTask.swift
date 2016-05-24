e8c8d41ec3c8bc322ac5aed5140e5b008128726c
yeltsinlobato <yeltsin.lobato@gmail.com>
2016-05-24 18:00:49 -0300

    Add ResearchKit Framework

---
+++ researchKitDesafio/AudioGeneratorTask.swift
@@ -0,0 +1,16 @@
+//
+//  AudioGeneratorTask.swift
+//  hearingCare
+//
+//  Created by Yeltsin Suares Lobato Gama on 5/24/16.
+//  Copyright © 2016 Lucas Freitas de Oliveira. All rights reserved.
+//
+
+import ResearchKit
+
+public var AudioGeneratorTask: ORKOrderedTask {
+    
+    return ORKOrderedTask.audioTaskWithIdentifier("AudioTask", intendedUseDescription: "A sentence prompt will be given to you to read.", speechInstruction: "These are the last dying words of Joseph of Aramathea", shortSpeechInstruction: "The Holy Grail can be found in the Castle of Aaaaaaaaaaah", duration: 10, recordingSettings: nil, options: ORKPredefinedTaskOption.None)
+    
+    
+}
---
+++ researchKitDesafio/ResearchKit-master/Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Bridging-Header .h
+//  researchKitDesafio
+//
+//  Created by Yeltsin Suares Lobato Gama on 5/24/16.
+//  Copyright © 2016 Lucas Freitas de Oliveira. All rights reserved.
+//
+
+#ifndef Bridging_Header__h
+#define Bridging_Header__h
+
+
+#endif /* Bridging_Header__h */
