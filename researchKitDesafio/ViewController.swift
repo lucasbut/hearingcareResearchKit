04956acbe23dab753cccb6cacf851a29334df4c5
LucasFreitas <lucas_but@hotmail.com>
2016-05-24 14:57:09 -0300

    Initial Commit

---
+++ researchKitDesafio/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  researchKitDesafio
+//
+//  Created by Lucas Freitas de Oliveira on 5/24/16.
+//  Copyright © 2016 Lucas Freitas de Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
